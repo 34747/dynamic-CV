2ffc82c8060a8d416c47b03cac741d16484f82a2
Sami Hasan <38310122+34747@users.noreply.github.com>
2019-08-25 22:08:21 +0600

    Add files via upload

---
+++ resumesami.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+<title>CV.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-blue w3-card-4">
+        <div class="w3-display-container">
+          <img src="sami.jpg" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-white">
+            <h2>Sami Hassan</h2>
+          </div>
+        </div>
+        <div class="w3-container">
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-blue"></i>Web Developer</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-blue"></i>Dhaka,Bangladesh</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-blue"></i>samihasan0@gmail.com</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-blue"></i>+8801726799779</p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-blue"></i>Skills</b></p>
+          <p>Theme development</p>
+          <div class="w3-light-grey w3-square-xlarge w3-small">
+            <div class="w3-container w3-center w3-square-xlarge w3-blue" style="width:90%">90%</div>
+          </div>
+          <p>Php</p>
+          <div class="w3-light-grey w3-square-xlarge w3-small">
+            <div class="w3-container w3-center w3-square-xlarge w3-blue" style="width:80%">
+              <div class="w3-center w3-text-white">80%</div>
+            </div>
+          </div>
+          <p>HTML5</p>
+          <div class="w3-light-grey w3-square-xlarge w3-small">
+            <div class="w3-container w3-center w3-square-xlarge w3-blue" style="width:80%">80%</div>
+          </div>
+          <p>CSS3</p>
+          <div class="w3-light-grey w3-square-xlarge w3-small">
+            <div class="w3-container w3-center w3-square-xlarge w3-blue" style="width:75%">75%</div>
+          </div>
+          <br>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-blue"></i>Languages</b></p>
+          <p>English</p>
+          <div class="w3-light-grey w3-square-xlarge">
+            <div class="w3-square-xlarge w3-blue" style="height:24px;width:100%"></div>
+          </div>
+          <p>Spanish</p>
+          <div class="w3-light-grey w3-square-xlarge">
+            <div class="w3-square-xlarge w3-blue" style="height:24px;width:55%"></div>
+          </div>
+          <p>Bangla</p>
+          <div class="w3-light-grey w3-square-xlarge">
+            <div class="w3-square-xlarge w3-blue" style="height:24px;width:100%"></div>
+          </div>
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-blue w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-blue"></i>Work Experience</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity w3-text-blue"><b>Theme Developer/Brothers Company Ltd</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>August-2019 - <span class="w3-tag w3-blue w3-round">Current</span></h6>
+          <p>In this company, i am working as main theme developer here as i am very experienced at web development so i work for this particular task </p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity w3-text-blue"><b>Translator</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2017 - Dec 2018</h6>
+          <p>I worked in this company as a translator for chinese people.</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity w3-text-blue"><b>Marketting</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jun 2014 - Jan 2017</h6>
+          <p>I worked for markettig site for job experience </p><br>
+        </div>
+      </div>
+
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text- w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-blue"></i>Education</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity w3-text-blue"><b>Daffodil International School</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2011 - 2013</h6>
+          <p> SSC</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity w3-text-blue"><b>BAF Shaheen college,Dhaka</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015</h6>
+          <p>HSC</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity w3-text-blue"><b>Daffodil International University</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Current</h6>
+          <p>Bachelor Degree</p><br>
+        </div>
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+<footer class="w3-container w3-teal w3-center w3-margin-top">
+  <p>Find me on social media.</p>
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+</footer>
+
+</body>
+</html>
